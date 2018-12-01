99bb889e94b7d14dd154917a5ae0c8efe7c5e235
AnssK <anss.khaled00@gmail.com>
2018-12-02 01:06:10 +0200

    updating files with docs

--- index.php
+++ index.php
@@ -1,10 +1,24 @@
 <?php
 
+/**
+ * NOTS
+ * $_SESSION['loggedinUser'] an object of User model that contain current loggedin user data from user table there is field called acc_type
+ * acc_type = 0 -> Patient
+ * acc_type = 1 -> Dr
+ */
+
+
+
 require_once './init.php';
-require_once './home.php';
+
+
 ?>
 
-
+<div class="container-fluid px-0">
+    <?php
+        require_once './home.php';
+    ?>
+</div>
 
 
 <?php
